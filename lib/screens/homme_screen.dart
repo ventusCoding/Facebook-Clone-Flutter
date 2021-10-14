8510e26babc4cd7bee06992bb4d69bd36bf2ac85
Anwer Baccar <anwer.baccar.pro@gmail.com>
2021-10-14 18:37:51 +0100

    finishing app bar

--- lib/screens/homme_screen.dart
+++ lib/screens/homme_screen.dart
@@ -1,10 +1,39 @@
+import 'package:facebook_clone/config/palette.dart';
+import 'package:facebook_clone/widgets/circle_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            brightness: Brightness.light,
+            backgroundColor: Colors.white,
+            title: Text(
+              'facebook',
+              style: const TextStyle(
+                color: Palette.facebookBlue,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2,
+              ),
+            ),
+            centerTitle: false,
+            floating: true,
+            actions: [
+              CircleButton(null, Icons.search, 25.0, () => {print('Search')}),
+              CircleButton(null, MdiIcons.facebookMessenger, 25.0,
+                  () => {print('Messenger')})
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
